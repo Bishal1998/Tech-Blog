204f7b8b6f90410af125095481bc2471e722cffd
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-01-13 19:50:02 +0545

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { DesktopLogo, LaptopLogo, MobileLogo } from './assets/Logo'
+
+const App = () => {
+  return (
+    <div className='text-yellow-55 text-9xl font-inter bg-dark-8'>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "white": "#FFF",
+        "black": "000",
+        "yellow": {
+          55: "#FFD11A",
+          60: "#FFD633",
+          70: "#FFE066",
+          80: "#FFEB99",
+          90: "FFF5CC",
+          95: "FFFAE5",
+          97: "#FFFCF0",
+          99: "#FBFBFE"
+        },
+        "dark": {
+          8: "#141414",
+          10: "#1A1A1A",
+          15: "#262626",
+          20: "#333333",
+          25: "#404040",
+          30: "#4D4D4D",
+          35: "#595959",
+          40: "#666666"
+        },
+        "gray": {
+          50: "#7E7E81",
+          60: "#98989A",
+          70: "#B3B3B3",
+          80: "#CCCCCC",
+          90: "#E4E4E7",
+          95: "#F1F1F3",
+          97: "#F7F7F8",
+          99: "#FCFCFD",
+        },
+      },
+      fontFamily: {
+        "kumbh": ['Kumbh Sans', 'sans-serif'],
+        "inter": ['Inter', "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+

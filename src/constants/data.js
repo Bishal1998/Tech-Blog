73e25f53deb2077d24487d00723312ab8e46acd4
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-01-13 21:13:00 +0545

    Desktop Navbar

---
+++ src/constants/data.js
@@ -0,0 +1,28 @@
+const navItems = [
+    {
+        id: 1,
+        title: "Home",
+        link: "#",
+        active: true
+    },
+    {
+        id: 2,
+        title: "News",
+        link: "#",
+        active: false
+    },
+    {
+        id: 3,
+        title: "Podcasts",
+        link: "#",
+        active: false
+    },
+    {
+        id: 4,
+        title: "Resources",
+        link: "#",
+        active: false
+    },
+]
+
+export { navItems }

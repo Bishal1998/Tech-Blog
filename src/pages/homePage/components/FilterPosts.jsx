95738131b77a4a007c805f3534fa2e9477844517
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-01-21 12:28:43 +0545

    Responsive and Functional Filter

--- src/pages/homePage/components/FilterPosts.jsx
+++ src/pages/homePage/components/FilterPosts.jsx
@@ -1,32 +1,46 @@
-import React, { useCallback, useState } from 'react'
-import { Title } from '../constants/components'
-import { filterPosts } from '../constants/data';
+import React, { useCallback, useState } from "react";
+import { Title } from "../constants/components";
+import { filterPosts } from "../constants/data";
 
 const FilterPosts = () => {
+  const [selected, setSelected] = useState("All");
 
-    const [selected, setSelected] = useState("All");
+  console.log(selected);
+  return (
+    <section>
+      <Title
+        title="A Knowledge Treasure Trove"
+        link="blogs"
+        desc="Explore FutureTech' s In-Depth Blog Posts"
+        content="Blogs"
+      />
+      <div className="w-full px-6 py-6 lg:px-20 lg:py-16 flex justify-between gap-4 overflow-x-scroll xl:overflow-hidden">
+        <p
+          className={`lg:py-6 lg:px-5 2xl:py-[30px] 2xl:px-6 py-[18px] px-4 rounded-[7px] border font-inter text-sm 2xl:text-lg text-gray-60 cursor-pointer border-dark-15 ${
+            selected === "All" ? "bg-dark-10" : "bg-dark-15"
+          } w-48 flex justify-center items-center h-20`}
+          onClick={() => setSelected("All")}
+        >
+          All
+        </p>
+        {filterPosts.map((cat) => {
+          const { id, category } = cat;
 
-    console.log(selected)
-    return (
-        <section>
-            <Title title='A Knowledge Treasure Trove' link='blogs' desc="Explore FutureTech' s In-Depth Blog Posts" content='Blogs' />
-            <div className='w-full px-6 py-6 lg:px-20 lg:py-16 flex justify-between'>
-                <p className='py-6 px-5 rounded-[7px] border border-dark-15 font-inter text-sm text-gray-60 cursor-pointer bg-dark-10 w-[15%] flex justify-center items-center' onClick={() => setSelected("All")} >All</p>
-                {
-                    filterPosts.map((cat) => {
-                        const { id, category } = cat;
-
-                        return (
-                            <div key={id} className='py-6 px-5 rounded-[7px] border border-dark-15 font-inter text-sm text-gray-60 cursor-pointer flex justify-center items-center w-[15%]' onClick={() => setSelected(category)}>
-                                <p>{category}</p>
-                            </div>
-                        )
-                    })
-                }
+          return (
+            <div
+              key={id}
+              className={`lg:py-6 lg:px-5 2xl:py-[30px] 2xl:px-6 py-[18px] px-4 rounded-[7px] border border-dark-15 ${
+                selected === category ? "bg-dark-10" : "bg-dark-15"
+              } font-inter text-sm 2xl:text-lg text-gray-60 cursor-pointer flex justify-center items-center w-60 h-20 whitespace-nowrap`}
+              onClick={() => setSelected(category)}
+            >
+              <p>{category}</p>
             </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-        </section>
-    )
-}
-
-export default FilterPosts
+export default FilterPosts;

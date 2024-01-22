99aeda162b19b2e3faaf164f6df4c257a52b4430
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-01-22 15:33:40 +0545

    Reusable Resource Component and Complete Resources Comp

--- src/pages/homePage/components/Resources.jsx
+++ src/pages/homePage/components/Resources.jsx
@@ -2,10 +2,42 @@ import React from 'react'
 import { Resource, Title } from '../constants/components'
 
 const Resources = () => {
+
+    const ebooksLogo = () => {
+        return <svg xmlns="http://www.w3.org/2000/svg" width="60" height="61" viewBox="0 0 60 61" fill="none">
+            <path d="M28.5 35C28.5 26.3674 53.6449 11.001 52.4594 16.7595C49.9819 28.7935 49.9819 41.2065 52.4594 53.2405C53.6449 58.999 28.5 43.6326 28.5 35Z" fill="#FFD11A" />
+            <path d="M31.5 26C31.5 34.6326 6.35514 49.999 7.54064 44.2405C10.0181 32.2065 10.0181 19.7935 7.54064 7.75952C6.35514 2.00102 31.5 17.3674 31.5 26Z" fill="#404040" />
+        </svg>
+    }
+
+    const whitepaperLogo = () => {
+        return <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60" fill="none">
+            <path d="M30 6V54L9 42V18L30 6Z" fill="#FFD11A" />
+            <path d="M51 6V54L30 42V18L51 6Z" fill="#404040" />
+        </svg>
+    }
+
     return (
         <section>
             <Title title="Your Gateway to In-Depth Information" link="resources" desc="Unlock Valuable Knowledge with FutureTech's Resources" content="Resources" />
-            <Resource />
+            <Resource logo={ebooksLogo}
+                title="Ebooks"
+                desc="Explore our collection of ebooks covering a wide spectrum of future technology topics."
+                link="ebooks"
+                topic="Variety of Topics"
+                topicDesc="Topics include AI in education (25%), renewable energy (20%), healthcare (15%), space exploration (25%), and biotechnology (15%)."
+                img="https://images.pexels.com/photos/3761306/pexels-photo-3761306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                titleCount={100}
+                titleExpertise="Ebooks are authored by renowned experts with an average of 15 years of experience" />
+            <Resource logo={whitepaperLogo}
+                title="Whitepapers"
+                desc="Dive into comprehensive reports and analyses with our collection of whitepapers. "
+                link="whitepaper"
+                topic="Topics Coverage"
+                topicDesc="Whitepapers cover quantum computing (20%), AI ethics (15%), space mining prospects (20%), AI in healthcare (15%), and renewable energy strategies (30%)."
+                img="https://images.pexels.com/photos/1261824/pexels-photo-1261824.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                titleCount={50}
+                titleExpertise="Whitepapers are authored by subject matter experts with an average of 20 years of experience." />
         </section>
     )
 }
--- src/pages/homePage/constants/components/Resource.jsx
+++ src/pages/homePage/constants/components/Resource.jsx
@@ -5,19 +5,26 @@ import { FaEye } from "react-icons/fa";
 
 
 
-const Resource = () => {
+const Resource = ({
+    logo: LogoComponent,
+    title,
+    desc,
+    link,
+    topic,
+    topicDesc,
+    img,
+    titleCount,
+    titleExpertise
+}) => {
     return (
-        <article className='flex flex-col 2xl:flex-row items-start 2xl:items-center justify-between w-full px-6 py-6 lg:px-20 lg:py-16 gap-24 border border-dark-15 '>
+        title && <article className='flex flex-col 2xl:flex-row items-start 2xl:items-center justify-between w-full px-6 py-6 lg:px-20 lg:py-16 gap-24 border border-dark-15 '>
             <div className='w-full 2xl:w-[30%]'>
                 <div className='space-y-6'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="61" viewBox="0 0 60 61" fill="none">
-                        <path d="M28.5 35C28.5 26.3674 53.6449 11.001 52.4594 16.7595C49.9819 28.7935 49.9819 41.2065 52.4594 53.2405C53.6449 58.999 28.5 43.6326 28.5 35Z" fill="#FFD11A" />
-                        <path d="M31.5 26C31.5 34.6326 6.35514 49.999 7.54064 44.2405C10.0181 32.2065 10.0181 19.7935 7.54064 7.75952C6.35514 2.00102 31.5 17.3674 31.5 26Z" fill="#404040" />
-                    </svg>
-                    <p className='font-kumbh text-white text-3xl font-semibold'>Ebooks</p>
-                    <p className='font-inter text-gray-60 text-base'>Explore our collection of ebooks covering a wide spectrum of future technology topics.</p>
-                    <Link to='/ebooks' className='flex items-center justify-center gap-2 rounded-md border border-dark-15 bg-dark-10 py-4 px-5 w-full text-gray-60 font-inter text-sm' >
-                        Download Ebooks Now
+                    {LogoComponent()}
+                    <p className='font-kumbh text-white text-3xl font-semibold'>{title}</p>
+                    <p className='font-inter text-gray-60 text-base'>{desc}</p>
+                    <Link to={`/${link}`} className='flex items-center justify-center gap-2 rounded-md border border-dark-15 bg-dark-10 py-4 px-5 w-full text-gray-60 font-inter text-sm' >
+                        Download {title} Now
                         <GoArrowUpRight size={20} color='yellow' />
                     </Link>
                 </div>
@@ -29,14 +36,14 @@ const Resource = () => {
             <div className='w-full 2xl:w-0 h-[1px] 2xl:h-0 bg-dark-15' />
             <div className='flex flex-col gap-6'>
                 <div className='flex flex-col lg:flex-row items-start lg:items-center justify-center gap-8'>
-                    <p className='text-white font-inter text-xl font-semibold whitespace-nowrap'>Variety of Topics</p>
-                    <p className='text-gray-60 font-inter text-base'>Topics include AI in education (25%), renewable energy (20%), healthcare (15%), space exploration (25%), and biotechnology (15%).</p>
+                    <p className='text-white font-inter text-xl font-semibold whitespace-nowrap'>{topic}</p>
+                    <p className='text-gray-60 font-inter text-base'>{topicDesc}</p>
                 </div>
-                <img src="https://images.pexels.com/photos/3761306/pexels-photo-3761306.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" height={275} className='w-full h-[275px] rounded-[10px] object-cover bg-bottom' />
+                <img src={img} alt={title} height={275} className='w-full h-[275px] rounded-[10px] object-cover bg-bottom' />
                 <div className='flex flex-col 2xl:flex-row items-center justify-between gap-4'>
                     <div className='p-6 bg-dark-10 border border-dark-15 rounded-lg font-inter w-full 2xl:w-1/4'>
-                        <p className='text-gray-60 text-base'>Total Ebooks</p>
-                        <p className='text-white text-lg font-semibold'>Over 100 ebooks</p>
+                        <p className='text-gray-60 text-base'>Total {title}</p>
+                        <p className='text-white text-lg font-semibold'>Over {titleCount} {title.toLowerCase()}</p>
                     </div>
                     <div className='bg-dark-10  border border-dark-15 rounded-lg font-inter flex justify-between items-center p-6 flex-1 w-full'>
                         <div>
@@ -51,7 +58,7 @@ const Resource = () => {
                 </div>
                 <div className='rounded-lg bg-dark-10 border border-dark-15 p-6'>
                     <p className='font-inter text-base text-gray-60'>Average Author Expertise</p>
-                    <p className='font-inter text-lg font-medium text-white'>Ebooks are authored by renowned experts with an average of 15 years of experience</p>
+                    <p className='font-inter text-lg font-medium text-white'>{titleExpertise}</p>
                 </div>
             </div>
         </article>

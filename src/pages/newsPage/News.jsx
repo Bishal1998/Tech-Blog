52b38946ff4408cae723082bbc7c44d2ef32e78a
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-01-26 08:44:36 +0545

    First News Done

--- src/pages/newsPage/News.jsx
+++ src/pages/newsPage/News.jsx
@@ -3,11 +3,17 @@ import { FilterPosts } from "../homePage/components"
 import { Title } from '../homePage/constants/components'
 import { MultipleNews, Videos } from './components'
 import { Headline, NewsComp } from "../../constants/components"
+import FirstNews from './components/FirstNews'
+import { newsItems } from './data'
+
+
+console.log(newsItems[0])
 
 const News = () => {
     return (
         <section>
             <Headline title="Today's Headlines: Stay" subtitle="Informed" desc="Explore the latest news from around the world. We bring you up-to-the-minute updates on the most significant events, trends, and stories. Discover the world through our news coverage." />
+            <FirstNews news={newsItems[0]} />
             <MultipleNews />
             <FilterPosts />
             <Title title="Featured Videos"

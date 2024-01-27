af65245b9d975719be37af27732bc6e71306c0fe
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-01-27 19:54:54 +0545

    DB Connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+import connectToDb from "./db/index.js";
 
 configDotenv();
 
 const app = express();
 const PORT = process.env.PORT;
+connectToDb();
 
 
 app.listen(PORT, () => {

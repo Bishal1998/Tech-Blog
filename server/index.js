cb8b268407685c4e31534bac3215b7d5919849a8
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-01-27 21:17:40 +0545

    signup route created

--- server/index.js
+++ server/index.js
@@ -2,14 +2,17 @@ import express from "express";
 import { configDotenv } from "dotenv";
 import connectToDb from "./db/index.js";
 import userRoutes from "./routes/User.js";
+import signupRoutes from "./routes/Signup.js";
 
 configDotenv();
 
 const app = express();
 const PORT = process.env.PORT;
 connectToDb();
+app.use(express.json());
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", signupRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`)

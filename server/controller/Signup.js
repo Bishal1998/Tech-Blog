dceab34d963820f02e2e5e0784e1971865b41b59
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-01-27 22:31:49 +0545

    middleware and errorHandler

--- server/controller/Signup.js
+++ server/controller/Signup.js
@@ -1,11 +1,12 @@
 import User from "../models/User.js";
 import bcyptjs from "bcryptjs";
+import { errorHandler } from "../utils/Error.js";
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === "" || email === "" || password === "") {
-        return res.status(400).json({ message: "All fields are required" })
+        next(errorHandler(400, 'All fields are required'));
     }
 
     const hashedPassword = bcyptjs.hashSync(password, 10);
@@ -16,7 +17,7 @@ const signup = async (req, res) => {
         await newUser.save();
         res.json({ message: "User created successfully" })
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        next(error);
     }
 }
 
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import { configDotenv } from "dotenv";
 import connectToDb from "./db/index.js";
 import userRoutes from "./routes/User.js";
 import signupRoutes from "./routes/Signup.js";
+import { errorMiddleware } from "./middleware/Middleware.js";
 
 configDotenv();
 
@@ -11,9 +12,13 @@ const PORT = process.env.PORT;
 connectToDb();
 app.use(express.json());
 
+
 app.use("/api/user", userRoutes);
 app.use("/api/auth", signupRoutes);
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`)
 })
+
+export default app;

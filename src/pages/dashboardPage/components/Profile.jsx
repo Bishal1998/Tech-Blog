45b837d6d6162cf8de6447366adc8b88ea4ec597
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-01-30 18:37:47 +0545

    Profile Image saved in Firebase

--- src/pages/dashboardPage/components/Profile.jsx
+++ src/pages/dashboardPage/components/Profile.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Input } from '../../../constants/components'
 import { useSelector } from 'react-redux'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage"
+import app from '../../../Firebase'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Profile = () => {
 
     const { currentUser } = useSelector(state => state.auth)
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
+    const [imageFileUploadStatus, setImageFileUploadStatus] = useState(null);
+    const [imageFileUploadError, setImageFileUploadError] = useState(null);
     const imagePickerRef = useRef();
 
     const handleImageChanger = (e) => {
@@ -23,8 +29,31 @@ const Profile = () => {
         }
     }, [imageFile])
 
+
+    // firebase media upload
     const uploadImage = async () => {
-        console.log("uploadig")
+
+        setImageFileUploadError(null);
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + imageFile.name;
+        const storageRef = ref(storage, fileName);
+        const uploadTask = uploadBytesResumable(storageRef, imageFile);
+
+        uploadTask.on("state_changed", (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setImageFileUploadStatus(progress.toFixed(0))
+        },
+            (error) => {
+                setImageFileUploadError("Couldn't upload file (File Must be Less Than 2 MB");
+
+                setImageFileUploadStatus(null);
+                setImageFile(null);
+                setImageFileUrl(null);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => setImageFileUrl(downloadURL))
+            }
+        )
     }
 
     return (
@@ -32,8 +61,30 @@ const Profile = () => {
             <h2 className='font-medium text-white font-inter text-4xl'>Profile</h2>
 
             <div className='min-h-screen w-[90%] flex flex-col items-center justify-start mt-12'>
-                <form className='w-full flex flex-col items-center'>
-                    <img src={imageFileUrl || currentUser.profilePicture} alt={currentUser.username} className='rounded-full w-40 h-40 object-cover border-[10px] border-dark-30 self-center cursor-pointer' onClick={() => imagePickerRef.current.click()} />
+                <form className='w-full flex flex-col items-center gap-4'>
+
+                    <div className='relative'>
+                        <img src={imageFileUrl || currentUser.profilePicture} alt={currentUser.username} className={`rounded-full w-40 h-40 object-cover border-[10px] border-dark-30 self-center cursor-pointer ${imageFileUploadStatus && imageFileUploadStatus < 100 && "opacity-60"}`} onClick={() => imagePickerRef.current.click()} />
+                        {
+                            imageFileUploadStatus && (
+                                <CircularProgressbar value={imageFileUploadStatus || 0} text={`${imageFileUploadStatus}%`} strokeWidth={5} styles={{
+                                    root: {
+                                        width: '160px',
+                                        height: '160px',
+                                        position: 'absolute',
+                                        top: 0,
+                                        left: 0,
+                                    },
+                                    path: {
+                                        stroke: `rgba(62,152,199, ${imageFileUploadStatus / 100})`
+                                    }
+                                }} />
+                            )
+                        }
+                    </div>
+                    {
+                        imageFileUploadError && <p className='py-1 px-2 lg:py-[6px] lg:px-[10px] bg-red-600 rounded-[4px] text-base lg:text-xl font-medium font-inter text-white w-fit'>{imageFileUploadError}</p>
+                    }
                     <input type="file" accept='image/*' onChange={handleImageChanger} ref={imagePickerRef} className='hidden' />
                     <Input title="Username" type="email" name="username" value={currentUser.username} />
                     <Input title="Email" type="email" name="email" value={currentUser.email} />

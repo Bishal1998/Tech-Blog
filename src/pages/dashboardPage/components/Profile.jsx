07c4575d47b4ff332b268c47a806f82cf835a106
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-01-30 15:10:09 +0545

    Profile Functionality

--- src/pages/dashboardPage/components/Profile.jsx
+++ src/pages/dashboardPage/components/Profile.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Input } from '../../../constants/components'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+
+    const { currentUser } = useSelector(state => state.auth)
+
     return (
         <section className='w-full pt-4 flex flex-col justify-center items-center'>
-            <h2 className='font-medium text-white font-inter text-xl'>Profile</h2>
+            <h2 className='font-medium text-white font-inter text-4xl'>Profile</h2>
 
             <div className='min-h-screen w-[90%] flex flex-col items-center justify-start mt-12'>
-                <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='rounded-full w-40 h-40 object-cover border-[10px] border-dark-30' />
+                <img src={currentUser.profilePicture} alt={currentUser.username} className='rounded-full w-40 h-40 object-cover border-[10px] border-dark-30 self-center cursor-pointer' />
                 <form className='w-full flex flex-col items-center'>
-                    <Input title="Username" type="email" name="username" value="username" />
-                    <Input title="Email" type="email" name="email" value="Email" />
-                    <Input title="Password" type="text" name="password" value="Password" />
+                    <Input title="Username" type="email" name="username" value={currentUser.username} />
+                    <Input title="Email" type="email" name="email" value={currentUser.email} />
+                    <Input title="Password" type="text" name="password" value={currentUser.password} />
                     <button className='w-full sm:w-[500px] p-5 rounded-lg border-dark-15 bg-yellow-55 font-inter text-xl text-dark-8 hover:bg-yellow-60 font-medium' type='submit'>
                         Update
                     </button>

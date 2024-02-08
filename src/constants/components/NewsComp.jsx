d2644523446053899d11ceea5b966f42a4e3ad63
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-02-08 15:34:46 +0545

    News Component Modified

--- src/constants/components/NewsComp.jsx
+++ src/constants/components/NewsComp.jsx
@@ -1,40 +1,42 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 import { GoArrowUpRight } from "react-icons/go";
-import {
-    FaHeart,
-    FaRegHeart,
-    FaRegComment,
-} from "react-icons/fa";
+import { FaHeart, FaRegHeart, FaRegComment } from "react-icons/fa";
+import { useCommentCount } from "../../Custom";
 
+const NewsComp = ({ image, title, category, slug, likesCount, postId }) => {
+  const commentCount = useCommentCount(postId);
+  return (
+    <article className="space-y-4 w-full h-full">
+      <img
+        src={image}
+        alt={title}
+        height={222}
+        className="w-full rounded-xl h-[222px] object-cover"
+      />
+      <div className="font-inter text-xl ">
+        <p className="text-white font-semibold">{title}</p>
+        <p className="text-gray-60 py-2 capitalize">{category}</p>
+      </div>
+      <div className="flex gap-4 whitespace-nowrap font-kumbh text-sm 2xl:text-lg text-gray-60">
+        <p className="flex items-center justify-center gap-2 py-2 px-4 rounded-[100px] bg-dark-10 border border-dark-15 cursor-pointer">
+          <FaHeart size={24} color="orange" />
+          {likesCount > 999 ? `${likesCount / 1000}k` : likesCount}
+        </p>
+        <p className="flex items-center justify-center gap-2 py-2 px-4 rounded-[100px] bg-dark-10 border border-dark-15 cursor-pointer">
+          <FaRegComment size={24} />
+          {commentCount > 999 ? `${commentCount / 1000}k` : commentCount}
+        </p>
+        <Link
+          to={`/posts/${slug}`}
+          className="flex items-center justify-center py-4 px-6 rounded-xl border border-dark-15 bg-dark-8 font-inter text-lg text-gray-60 gap-2"
+        >
+          Read More
+          <GoArrowUpRight size={24} color="yellow" />
+        </Link>
+      </div>
+    </article>
+  );
+};
 
-const NewsComp = ({ image, title, category, slug }) => {
-
-    return (
-        <article className='space-y-4 w-full h-full'>
-            <img src={image} alt={title} height={222} className='w-full rounded-xl h-[222px] object-cover' />
-            <div className='font-inter text-xl '>
-                <p className='text-white font-semibold'>{title}</p>
-                <p className='text-gray-60 py-2 capitalize'>{category}</p>
-            </div>
-            <div className="flex gap-4 whitespace-nowrap font-kumbh text-sm 2xl:text-lg text-gray-60">
-                <p className="flex items-center justify-center gap-2 py-2 px-4 rounded-[100px] bg-dark-10 border border-dark-15 cursor-pointer">
-                    <FaHeart size={24} color="orange" />
-                    30k
-                    {/* {likeCount > 999 ? `${likeCount / 1000}k` : likeCount} */}
-                </p>
-                <p className="flex items-center justify-center gap-2 py-2 px-4 rounded-[100px] bg-dark-10 border border-dark-15 cursor-pointer">
-                    <FaRegComment size={24} />
-                    20k
-                    {/* {commentCount > 999 ? `${commentCount / 1000}k` : commentCount} */}
-                </p>
-                <Link to={`/posts/${slug}`} className='flex items-center justify-center py-4 px-6 rounded-xl border border-dark-15 bg-dark-8 font-inter text-lg text-gray-60 gap-2'>
-                    Read More
-                    <GoArrowUpRight size={24} color='yellow' />
-                </Link>
-            </div>
-        </article>
-    )
-}
-
-export default NewsComp
+export default NewsComp;
--- src/pages/newsPage/components/MultipleNews.jsx
+++ src/pages/newsPage/components/MultipleNews.jsx
@@ -1,59 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import { newsItems } from '../data'
-import { NewsComp } from '../../../constants/components';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { newsItems } from "../data";
+import { NewsComp } from "../../../constants/components";
+import axios from "axios";
 
 const MultipleNews = () => {
+  const [posts, setPosts] = useState([]);
+  const [author, setAuthor] = useState({});
+  const [loading, setLoading] = useState(true);
 
-    const [posts, setPosts] = useState([])
-    const [author, setAuthor] = useState({})
-    const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get(`/api/post/getPosts`);
+        setPosts(res.data.posts);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.message);
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
 
-    useEffect(() => {
-        const fetchPosts = async () => {
+  const { title, category, content, updatedAt, image, slug, userId } = posts;
 
-            try {
-                const res = await axios.get(`/api/post/getPosts`);
-                setPosts(res.data.posts)
-                setLoading(false)
-            } catch (error) {
-                console.log(error.message)
-                setLoading(false)
-            }
+  useEffect(() => {
+    const fetchAuthor = async () => {
+      try {
+        const res = await axios.get(`/api/user/${userId}`);
+        setAuthor(res.data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchAuthor();
+  }, [userId]);
 
-        }
-        fetchPosts();
-    }, [])
-
-    const { title, category, content, updatedAt, image, slug, userId, } = posts;
-
-    useEffect(() => {
-        const fetchAuthor = async () => {
-            try {
-
-                const res = await axios.get(`/api/user/${userId}`)
-                setAuthor(res.data)
-
-            } catch (error) {
-                console.log(error.message)
-            }
-        }
-        fetchAuthor();
-    }, [userId])
-
-    if (loading) return <p className='font-kumbh text-gray-60 text-2xl font-medium text-center'>Loading....</p>
+  if (loading)
     return (
-        <section className='w-full px-6 py-6 lg:px-20 lg:py-16'>
-            <div className='flex flex-col xl:flex-row items-start justify-between gap-4'>
-                {
-                    posts?.slice(1, 4).map((post) => {
-                        const { _id, ...item } = post;
-                        return <NewsComp key={_id} {...item} />
-                    })
-                }
-            </div>
-        </section>
-    )
-}
+      <p className="font-kumbh text-gray-60 text-2xl font-medium text-center">
+        Loading....
+      </p>
+    );
+  return (
+    <section className="w-full px-6 py-6 lg:px-20 lg:py-16">
+      <div className="flex flex-col xl:flex-row items-start justify-between gap-4">
+        {posts?.slice(1, 4).map((post) => {
+          const { _id, ...item } = post;
+          return <NewsComp key={_id} postId={_id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default MultipleNews
+export default MultipleNews;

5d6636589fe4d14b0cc19078334e22ea83f3b1b8
Bishal1998 <Bishalpaudel1998@gmail.com>
2024-02-08 15:56:26 +0545

    Dynamic Title for Admin Dashboard

--- src/pages/dashboardPage/Dashboard.jsx
+++ src/pages/dashboardPage/Dashboard.jsx
@@ -1,48 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import { CreatePost, Lists, Profile, Sidebar } from './components';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { CreatePost, Lists, Profile, Sidebar } from "./components";
 
 const Dashboard = () => {
+  const [tab, setTab] = useState("");
+  const { currentUser } = useSelector((state) => state.auth);
+  const { search } = useLocation();
 
-    const [tab, setTab] = useState("");
-    const { currentUser } = useSelector(state => state.auth)
-    const { search } = useLocation();
+  useEffect(() => {
+    const urlParams = new URLSearchParams(search).get("tab");
+    if (urlParams) {
+      setTab(urlParams);
+    }
+  }, [search]);
 
-    useEffect(() => {
-        const urlParams = new URLSearchParams(search).get("tab")
-        if (urlParams) {
-            setTab(urlParams)
-        }
-    }, [search])
+  document.title = "Dashboard - Tech Blog";
 
+  return currentUser ? (
+    <section className="flex justify-center min-h-screen h-full">
+      <div className="hidden absolute left-0 lg:flex flex-col w-1/5 bg-dark-10 min-h-screen py-2 px-4">
+        <Sidebar />
+      </div>
+      {tab === "profile" && (
+        <div className="w-full lg:w-3/4 lg:ml-auto lg:mr-20">
+          <Profile />
+        </div>
+      )}
+      {tab === "create-post" && (
+        <div className="w-full lg:w-3/4 lg:ml-auto lg:mr-20">
+          <CreatePost />
+        </div>
+      )}
+      {tab === "posts" && (
+        <div className="w-full lg:w-3/4 lg:ml-auto lg:mr-20">
+          <Lists />
+        </div>
+      )}
+    </section>
+  ) : (
+    <Navigate to="/signup" />
+  );
+};
 
-    return currentUser ?
-        <section className='flex justify-center min-h-screen h-full'>
-            <div className='hidden absolute left-0 lg:flex flex-col w-1/5 bg-dark-10 min-h-screen py-2 px-4'>
-                <Sidebar />
-            </div>
-            {tab === "profile" &&
-                <div className='w-full lg:w-3/4 lg:ml-auto lg:mr-20'>
-                    <Profile />
-                </div>
-            }
-            {
-                tab === "create-post" &&
-                <div className='w-full lg:w-3/4 lg:ml-auto lg:mr-20'>
-                    <CreatePost />
-                </div>
-            }
-            {
-                tab === "posts" &&
-                <div className='w-full lg:w-3/4 lg:ml-auto lg:mr-20'>
-                    <Lists />
-                </div>
-            }
-        </section> :
-        <Navigate to="/signup" />
-
-}
-
-export default Dashboard
+export default Dashboard;
